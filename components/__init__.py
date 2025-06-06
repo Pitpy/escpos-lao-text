3459d7cff478eca4d1af282f5210835847fde2bc
pitpy <pitpy12@gmail.com>
2025-06-06 10:25:46 +0700

    new components init file

---
+++ components/__init__.py
@@ -0,0 +1,11 @@
+from .receipt_line import render_receipt_line
+from .item_line import render_item_line
+from .table_header import render_table_header
+from .total_line import render_total_line
+
+__all__ = [
+    'render_receipt_line',
+    'render_item_line', 
+    'render_table_header',
+    'render_total_line'
+]
